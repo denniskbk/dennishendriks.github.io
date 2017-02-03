72dc34921120e49ca5169119db6e1fd1ce199f7d
Dennis Hendriks <development@dennishendriks.com>
2017-02-03 15:33:32 +0100

    Fix react production build

--- Portfolio/webpack/config.build.js
+++ Portfolio/webpack/config.build.js
@@ -4,16 +4,13 @@ import base from './config.base';
 
 const config = base('development');
 
-// production overrides go here
-// config.plugins = config.plugins.concat([
-//     new webpack.optimize.OccurenceOrderPlugin(),
-//     new webpack.optimize.DedupePlugin(),
-//     new webpack.optimize.UglifyJsPlugin({
-//         compress: {
-//             warnings: false,
-//         },
-//         comments: false
-//     })
-// ]);
+config.plugins = config.plugins.concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+         NODE_ENV: JSON.stringify("production")
+       }
+   })
+]);
+
 
 export default config;
